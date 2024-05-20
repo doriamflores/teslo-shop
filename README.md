b5ad1381305e589410fc30e68fd70aa6016f1d01
black door <doriamfloreszorrilla@gmail.com>
2024-05-20 17:53:44 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Teslo API
+
+1. Clonar proyecto
+
+2. `npm install`
+
+3. Copiar el `.env.template` a `.env` y modificarlo.
+
+4. Cambiar las variables de entorno.
+
+5. Levantar la base de datos
+
+```
+docker-compose up -d
+```
+
+6. Ejecutar el proyecto
+
+```
+npm run start:dev
+```
---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,48 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  title: string;
+
+  @Column('numeric', {
+    default: 0,
+  })
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column({
+    type: 'text',
+    unique: true,
+  })
+  slug: string;
+
+  @Column({
+    type: 'int',
+    default: 0,
+  })
+  stock: number;
+
+  @Column({
+    type: 'text',
+    array: true,
+  })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
+
+  //tags
+  //images
+}
